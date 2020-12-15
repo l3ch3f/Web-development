510243f5fcad471547f230f8c066589fa4d14c3f
l3ch3f <mfsoeropaiman@gmail.com>
2020-12-15 12:38:11 +0100

    3e dag

---
+++ WebdevCourse/6-Java/wk1d2/wk1d2.java
@@ -0,0 +1,9 @@
+
+
+public class wk1d2 {
+    public static void main(String[] args) {
+        // TODO code applicaion logic
+        System.out.println("Mijn eerste programma!");
+        System.out.println("en mijn tweede regel in Java!");
+    }
+}
