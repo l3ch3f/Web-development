aaa48290cda4c0fa545bc74022337577fdfa8746
l3ch3f <mfsoeropaiman@gmail.com>
2021-01-27 20:51:26 +0100

    Flexible lists

--- WebdevCourse/7-React/react-learning/src/App.js
+++ WebdevCourse/7-React/react-learning/src/App.js
@@ -25,14 +25,19 @@ class App extends Component {
     //     });
     // };
 
-    nameChangedHandler = (event) => {
-        this.setState({
-            persons: [
-                { name: "Max", age: 28 },
-                { name: event.target.value, age: 29 },
-                { name: "Stephanie", age: 26 },
-            ],
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.find(p => {
+          return p.id === id;
         });
+
+        const person = {
+          ...this.state.persons[personIndex]
+        };
+
+        person.name = event.target.value;
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+        this.setState({ persons: persons });
     };
 
     deletePersonHandler = (personIndex) => {
@@ -72,6 +77,7 @@ class App extends Component {
                                 name={person.name}
                                 age={person.age}
                                 key={person.id}
+                                changed={(event) => this.nameChangedHandler(event, person.id)}
                             />
                         );
                     })}
