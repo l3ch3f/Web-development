a6864a98c92bf5a882be82ec907de8aea1af79d1
l3ch3f <mfsoeropaiman@gmail.com>
2021-01-28 08:12:53 +0100

    Styleroot with mediaquery and wrapped the component

--- WebdevCourse/7-React/react-learning/src/App.js
+++ WebdevCourse/7-React/react-learning/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Radium from "radium"
+import Radium, { StyleRoot } from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -27,12 +27,12 @@ class App extends Component {
     // };
 
     nameChangedHandler = (event, id) => {
-        const personIndex = this.state.persons.findIndex(p => {
-          return p.id === id;
+        const personIndex = this.state.persons.findIndex((p) => {
+            return p.id === id;
         });
 
         const person = {
-          ...this.state.persons[personIndex]
+            ...this.state.persons[personIndex],
         };
 
         person.name = event.target.value;
@@ -56,15 +56,15 @@ class App extends Component {
     render() {
         const style = {
             backgroundColor: "green",
-            color: 'white',
+            color: "white",
             font: "inherit",
             border: "1px solid blue",
             padding: "8px",
             cursor: "pointer",
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
+            ":hover": {
+                backgroundColor: "lightgreen",
+                color: "black",
+            },
         };
 
         let persons = null;
@@ -83,7 +83,9 @@ class App extends Component {
                                 name={person.name}
                                 age={person.age}
                                 key={person.id}
-                                changed={(event) => this.nameChangedHandler(event, person.id)}
+                                changed={(event) =>
+                                    this.nameChangedHandler(event, person.id)
+                                }
                             />
                         );
                     })}
@@ -108,28 +110,31 @@ class App extends Component {
                 </div>
             );
 
-            style.backgroundColor = 'red';
-            style.[':hover'] = {
-                backgroundColor: 'yellow',
-                color: 'grey'
-            }
+            style.backgroundColor = "red";
+            style[":hover"] = {
+                backgroundColor: "yellow",
+                color: "grey",
+            };
         }
 
-        const classes = []; 
+        const classes = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red'); // classes = ['red']
-        } if (this.state.persons.length <= 1) {
-            classes.push('bold'); // classes = ['red', 'bold']
+            classes.push("red"); // classes = ['red']
+        }
+        if (this.state.persons.length <= 1) {
+            classes.push("bold"); // classes = ['red', 'bold']
         }
         return (
-            <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <button style={style} onClick={this.togglePersonsHandler}>
-                    Toggle Persons
-                </button>
-                {persons}
-            </div>
+            <StyleRoot>
+                <div className="App">
+                    <h1>Hi, I'm a React App</h1>
+                    <p className={classes.join(" ")}>This is really working!</p>
+                    <button style={style} onClick={this.togglePersonsHandler}>
+                        Toggle Persons
+                    </button>
+                    {persons}
+                </div>
+            </StyleRoot>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
