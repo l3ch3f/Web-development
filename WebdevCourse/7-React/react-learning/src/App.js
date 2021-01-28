8ff5b7308d3fd3bdbfe560a694caf23581bcce8a
l3ch3f <mfsoeropaiman@gmail.com>
2021-01-28 08:05:59 +0100

    added radium for psudo selector

--- WebdevCourse/7-React/react-learning/src/App.js
+++ WebdevCourse/7-React/react-learning/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Radium from "radium"
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -60,6 +61,10 @@ class App extends Component {
             border: "1px solid blue",
             padding: "8px",
             cursor: "pointer",
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }
         };
 
         let persons = null;
@@ -104,12 +109,22 @@ class App extends Component {
             );
 
             style.backgroundColor = 'red';
+            style.[':hover'] = {
+                backgroundColor: 'yellow',
+                color: 'grey'
+            }
         }
 
+        const classes = []; 
+        if (this.state.persons.length <= 2) {
+            classes.push('red'); // classes = ['red']
+        } if (this.state.persons.length <= 1) {
+            classes.push('bold'); // classes = ['red', 'bold']
+        }
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <p>This is really working!</p>
+                <p className={classes.join(' ')}>This is really working!</p>
                 <button style={style} onClick={this.togglePersonsHandler}>
                     Toggle Persons
                 </button>
@@ -120,4 +135,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
