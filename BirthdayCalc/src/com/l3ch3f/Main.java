ae644648a19e0f4e20c164ecad09c9127389bee5
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-12 14:22:30 +0100

    .

--- BirthdayCalc/src/com/l3ch3f/Main.java
+++ BirthdayCalc/src/com/l3ch3f/Main.java
@@ -3,7 +3,10 @@ package com.l3ch3f;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
+import java.util.Locale;
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
 
@@ -16,16 +19,22 @@ public class Main {
         System.out.println("What is your last name?");
         String lastName = scan.nextLine();
         System.out.println("------------");
-        System.out.println("Which year were you born?");
-        Integer year = scan.nextInt();
-        System.out.println("------------");
-        System.out.println("Which month were you born?");
-        System.out.println("Expected input: the number of the month: Es. 3=march");
-        Integer month = scan.nextInt();
-        System.out.println("------------");
-        System.out.println("On which day were you born?");
-        Integer day = scan.nextInt();
+        System.out.println("What is your day of birth ?");
+        System.out.println("Expected input: dd/mm/yyyy");
+        String birthDayInput = scan.next();
+//        Integer year = scan.nextInt();
+//        System.out.println("------------");
+//        System.out.println("Which month were you born?");
+//        System.out.println("Expected input: the number of the month: Es. 3=march");
+//        Integer month = scan.nextInt();
+//        System.out.println("------------");
+//        System.out.println("On which day were you born?");
+//        Integer day = scan.nextInt();
+        String[] parts = birthDayInput.split("/");
 
+        int day = parseInt(parts[0]);
+        int month = parseInt(parts[1]);
+        int year = parseInt(parts[2]);
 
 
 
@@ -35,6 +44,7 @@ public class Main {
         Period p = Period.between(birthday,today); // eerste parameter ten opzichte van tweede parameter
 
         System.out.println("Hello and good day "+ firstName + " " + lastName + " you are " + p.getYears() + " old");
+        System.out.println("Zoveel dagen oud "+ firstName + " " + lastName + " you are " + p.getDays() + " old");
+        System.out.println("Zoveel maanden oud "+ firstName + " " + lastName + " you are " + p.getMonths()+ " old");
     }
 }
-dd/mm/yyyy
--- WebdevCourse/7-React/http--01-starting-setup/src/components/FullPost/FullPost.js
+++ WebdevCourse/7-React/http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -13,6 +13,7 @@ class FullPost extends Component {
         if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
             
         }
+        // Get requests
       axios
         .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
         .then(response => {
@@ -20,6 +21,15 @@ class FullPost extends Component {
           this.setState({ loadedPost: response.data });
         });
     }
+  }
+
+  // DELETE handling
+
+  deletePostHandler = () => {
+    axios.delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+    .then(response => {
+        console.log(response);  
+    });
   }
 
   render() {
@@ -33,7 +43,7 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className='Edit'>
-            <button className='Delete'>Delete</button>
+            <button className='Delete' onClick={this.deletePostHandler}>Delete</button>
           </div>
         </div>
       );
