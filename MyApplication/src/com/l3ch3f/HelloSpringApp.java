88312c63d3313b9c21c6eb4ff83fba21fea668b6
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-15 08:23:58 +0100

    .

---
+++ MyApplication/src/com/l3ch3f/HelloSpringApp.java
@@ -0,0 +1,16 @@
+package com.l3ch3f;
+
+
+public class HelloSpringApp {
+    public static <ClassPathXmlApplicationContext> void main(String[] args) {
+        // Load the spring config file
+        ClassPathXmlApplicationContext context;
+        context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+        // close the context
+        context.close();
+    }
+}
