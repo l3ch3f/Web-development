e65e7c8669d1a02925f986fc9aeea507c3c73e6c
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-16 18:40:45 +0100

    .

--- MethodsTG/src/com/l3ch3f/Animal.java
+++ MethodsTG/src/com/l3ch3f/Animal.java
@@ -1,2 +1,24 @@
-package com.l3ch3f;public class Animal {
+package com.l3ch3f;
+
+public class Animal {
+    private String name;
+    private Integer age;
+    static String type = "Animal";
+
+    public Animal(String name, Integer age, String type) {
+        this.name = name;
+        this.age = age;
+        Animal.type = type;
+    }
+
+
+
+    public void feed(){
+        System.out.println("Animal is being Fed");
+    }
+
+    public void vistiVet() {
+        System.out.println("Animal is visiting Vet");
+    }
+
 }
--- MethodsTG/src/com/l3ch3f/Bear.java
+++ MethodsTG/src/com/l3ch3f/Bear.java
@@ -1,2 +1,35 @@
-package com.l3ch3f;public class Bear {
+package com.l3ch3f;
+
+public class Bear extends Animal{
+    private static String type;
+
+    public Bear(String name, Integer age, String type) {
+        super(name, age,type);
+        this.type = type;
+    }
+
+    public static void main(String[] args) {
+        Bear bearYogi = new Bear("Yogi", 22,"BrownBear");
+        Bear bearBoeBoe = new Bear("Boeboe", 22,"PolarBear");
+        Animal.type = "Animal";
+//        Animal willy = new Animal("willy",13,"");
+        Bear[] beren = {bearBoeBoe, bearYogi};
+        Animal[] dieren = {bearBoeBoe,bearYogi};
+
+
+
+        System.out.println(bearYogi.type);
+        bearBoeBoe.feed();
+        bearYogi.showType();
+
+
+    }
+
+    public void showType(){
+        System.out.println(type);
+        System.out.println(super.type);
+    }
+
+
+
 }
