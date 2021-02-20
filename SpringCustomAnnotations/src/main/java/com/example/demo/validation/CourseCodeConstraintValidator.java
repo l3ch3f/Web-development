12baf4b65f411765d0b35c06a9802a703e4f32bf
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-20 23:08:41 +0100

    Custom Annotation to how to work with.

---
+++ SpringCustomAnnotations/src/main/java/com/example/demo/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,19 @@
+package com.example.demo.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+    @Override
+    public void initialize(CourseCode constraintAnnotation) {
+        coursePrefix = constraintAnnotation.value();
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        // boolean result:
+        return s.startsWith(coursePrefix);
+    }
+}
