fd921daec2a1edcc46c47b1307d2d26d4397b2dc
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-20 18:43:26 +0100

    component beans

--- demo-annotations/src/main/java/com/example/demo/SpringDemoAnnotationsApplication.java
+++ demo-annotations/src/main/java/com/example/demo/SpringDemoAnnotationsApplication.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringDemoAnnotationsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoAnnotationsApplication.class, args);
+		// read spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
+		// get the bean from spring container
+		Coach theCoach = context.getBean("tennisCoach",Coach.class);
+		// call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		// close the context
+		context.close();
 	}
 
 }
---
+++ demo-annotations/src/main/java/com/example/demo/TennisCoach.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+}
