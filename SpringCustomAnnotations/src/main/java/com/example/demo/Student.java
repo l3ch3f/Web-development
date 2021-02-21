85a4508cab39a19f0a50fba5742fb1e6584007cb
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-21 12:00:52 +0100

    .

---
+++ SpringCustomAnnotations/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.Id;
+
+@Entity
+
+public class Student {
+
+
+}
