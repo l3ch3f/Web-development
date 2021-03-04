00ffffa19df85e20ed82b7b6e173407c5125b508
l3ch3f <mfsoeropaiman@gmail.com>
2021-03-04 17:23:36 +0100

    ...

--- eindopdracht/src/com/l3ch3f/Main.java
+++ eindopdracht/src/com/l3ch3f/Main.java
@@ -1,8 +1,50 @@
 package com.l3ch3f;
+import com.l3ch3f.model.Student;
+import com.l3ch3f.model.User;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Student mcfergus = new Student(new User("Mc Fergus", new Date(2011, 11, 22)),"Surinam");
+        JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+        //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("View");
+        JMenu m3 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        mb.add(m3);
+        JMenuItem m11 = new JMenuItem("Open");
+        JMenuItem m22 = new JMenuItem("Save as");
+        m1.add(m11);
+        m1.add(m22);
+
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter Text");
+        JTextField tf = new JTextField(10); // accepts upto 10 characters
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        // Text Area at the Center
+        JTextArea ta = new JTextArea();
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
     }
 }
