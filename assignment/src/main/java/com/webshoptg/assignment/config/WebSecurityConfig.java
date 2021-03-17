2b732a50e08f444485fdb7c18603a76ca6eb6761
l3ch3f <mfsoeropaiman@gmail.com>
2021-03-17 20:01:34 +0100

    ..

--- assignment/src/main/java/com/webshoptg/assignment/config/WebSecurityConfig.java
+++ assignment/src/main/java/com/webshoptg/assignment/config/WebSecurityConfig.java
@@ -8,12 +8,34 @@ import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.Collections;
 
-@Configuration
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+//    @Autowired
+//    private DataSource dataSource;
+//    @Autowired
+//    private ProductService productService;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable().and()
+//
+//
+//                .authorizeRequests()
+//                .antMatchers("/list_users").authenticated()
+//                .antMatchers("/register").authenticated()
+//                .anyRequest().permitAll()
+//                .and().exceptionHandling()
+//                .and().sessionManagement()
+//                .and()
+//                .formLogin()
+//                .usernameParameter("email")
+//                .defaultSuccessUrl("/list_users")
+//                .permitAll()
+//                .and()
+//                .logout().logoutSuccessUrl("/").permitAll();
+//
+//    }
 
-public class WebSecurityConfig {
-    @Autowired
-    private DataSource dataSource;
-    @Autowired
-    private ProductService productService;
 
-}
+
