aea8a356a09b96125a2103978b3b46bd9dee1802
l3ch3f <mfsoeropaiman@gmail.com>
2021-05-26 20:09:28 +0200

    ..

---
+++ WebdevCourse/3-Javascript/functions/index.js
@@ -0,0 +1,30 @@
+
+
+var functie1 = () {
+  //code block global scope
+}; 
+
+const variable2 = 33;
+
+let functie2 = () {
+  //code block 2 blcok scope
+  functie1();
+  const functie4 = () {
+
+  };
+  const variable = 22;
+  functie4();
+  var functie5 = () {
+    //functie 4
+  }
+  let 
+  variable2;
+};
+
+
+
+functie5();
+
+const functie3 = () {
+  //code block block scope
+};
